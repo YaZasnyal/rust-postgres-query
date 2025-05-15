2c41cf9be634c9fccbd5e35e57d6f83d754e5ebb
Artem Vasiliev <bladegreat@yandex.ru>
2025-05-15 19:32:39 +0300

    revert change

--- postgres_query/src/client.rs
+++ postgres_query/src/client.rs
@@ -69,11 +69,6 @@ impl GenericClient for Client {
     }
 
     #[deny(unconditional_recursion)]
-    async fn prepare_static(&self, sql: &'static str) -> Result<Statement, SqlError> {
-        Client::prepare_static(self, sql).await
-    }
-
-    #[deny(unconditional_recursion)]
     async fn execute_raw<'a>(
         &'a self,
         statement: &Statement,
@@ -101,11 +96,6 @@ impl GenericClient for DpClient {
     }
 
     #[deny(unconditional_recursion)]
-    async fn prepare_static(&self, sql: &'static str) -> Result<Statement, SqlError> {
-        DpClientWrapper::prepare_static(self, sql).await
-    }
-
-    #[deny(unconditional_recursion)]
     async fn execute_raw<'a>(
         &'a self,
         statement: &Statement,
@@ -128,10 +118,6 @@ impl GenericClient for DpClient {
 impl GenericClient for Transaction<'_> {
     async fn prepare(&self, sql: &str) -> Result<Statement, SqlError> {
         Transaction::prepare(self, sql).await
-    }
-
-    async fn prepare_static(&self, sql: &'static str) -> Result<Statement, SqlError> {
-        Transaction::prepare_static(self, sql).await
     }
 
     async fn execute_raw<'a>(
